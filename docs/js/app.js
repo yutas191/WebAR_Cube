dab502b228144ad69cd6f3dc20d933c32763ddbe
yutas191 <yutas191@gmail.com>
2018-09-17 07:34:44 +0900

    Add WebAR_Cube

---
+++ docs/js/app.js
@@ -0,0 +1,134 @@
+var meshCube;
+var meshRocket;
+
+class App {
+	init() {
+		// ライト
+		var light = new THREE.DirectionalLight(0xFFFFFF);
+		light.position.set(0,0,2);
+		scene.add( light );
+		var ambientLight = new THREE.AmbientLight(0x888888);
+		scene.add( ambientLight );
+
+		// マーカ1
+		var marker1 = new THREE.Group();
+		var controls1 = new THREEx.ArMarkerControls(context, marker1, {type: "pattern",patternUrl: "./data/kanji.patt"});
+		scene.add(marker1);
+
+		// マーカ2
+		var marker2 = new THREE.Group();
+		var controls2 = new THREEx.ArMarkerControls(context, marker2, {type: "pattern",patternUrl: "./data/hiro.patt"});
+		scene.add(marker2);
+
+		// Cube
+		var geometryCube = new THREE.CubeGeometry(1, 1, 1);
+		var materialCube = new THREE.MeshNormalMaterial({transparent: true,opacity: 0.8,side:THREE.DoubleSide});
+		meshCube = new THREE.Mesh(geometryCube, materialCube);
+		meshCube.name = "cube";
+		meshCube.position.set(0, 0.5, 0);
+		marker1.add(meshCube);
+
+		// Rocket
+		loader.load("./model/rocketX.json", function(geometryRocket, materialRocket){
+			materialRocket = new THREE.MeshPhongMaterial({map:THREE.ImageUtils.loadTexture("./model/rocketX.png"),side:THREE.DoubleSide});
+			meshRocket = new THREE.Mesh(geometryRocket, materialRocket);
+			meshRocket.name = "rocket";
+			meshRocket.scale.set(0.3, 0.3, 0.3);
+			meshRocket.position.set(0, 0.5, 0);
+			marker2.add(meshRocket);
+		});
+	}
+
+	// タッチ
+	touch(objName) {
+		console.log(objName);
+		switch(objName) {
+		case "cube":
+			this.tweenCube();
+			break;
+		case "rocket":
+			this.tweenRocket();
+			break;
+		default:
+			break;
+		}
+	}
+
+	// アニメーション
+	tweenCube() {
+		var twIni1 = {posZ: 0, rotX: 0};			// 初期位置
+		var twFor1 = {posZ: -2, rotX: -Math.PI};	// 終端位置
+		var twVal1 = {posZ: 0, rotX: 0};			// 更新パラメータ
+
+		var tween = new TWEEN.Tween(twVal1)			// 「行き」のアニメーション
+		.to(twFor1, 2000)
+		.easing(TWEEN.Easing.Back.Out)
+		.onUpdate(function() {
+			meshCube.position.z = twVal1.posZ;
+			meshCube.rotation.x = twVal1.rotX;
+			meshText1.position.z = twVal1.posZ;
+		})
+		.onComplete(function() {
+			var tween = new TWEEN.Tween(twVal1)		// 「帰り」のアニメーションを実行
+			.to(twIni1, 2000)
+			.easing(TWEEN.Easing.Back.InOut)
+			.onUpdate(function() {
+				meshCube.position.z = twVal1.posZ;
+				meshCube.rotation.x = twVal1.rotX;
+				meshText1.position.z = twVal1.posZ;
+			})
+			.onComplete(function() {
+				// なにもしない
+			})
+			.delay(100)
+			.start();
+		})
+		.delay(0)
+		.start();
+	}
+
+	// アニメーション
+	tweenRocket() {
+		var twIni2 = {posY: 0, rotY: 0};			// 初期位置
+		var twFor2 = {posY: 2, rotY: 2*Math.PI};	// 終端位置
+		var twVal2 = {posY: 0, rotY: 0};			// 更新パラメータ
+
+		var tween = new TWEEN.Tween(twVal2)			// 「行き」のアニメーション
+		.to(twFor2, 2000)
+		.easing(TWEEN.Easing.Quadratic.InOut)
+		.onUpdate(function() {
+			meshRocket.position.y = twVal2.posY;
+			meshRocket.rotation.y = twVal2.rotY;
+			meshText2.position.y = twVal2.posY;
+		})
+		.onComplete(function() {
+			var tween = new TWEEN.Tween(twVal2)		// 「帰り」のアニメーションを実行
+			.to(twIni2, 3000)
+			.easing(TWEEN.Easing.Quintic.InOut)
+			.onUpdate(function() {
+				meshRocket.position.y = twVal2.posY;
+				meshRocket.rotation.y = twVal2.rotY;
+				meshText2.position.y = twVal2.posY;
+			})
+			.onComplete(function() {
+				// なにもしない
+			})
+			.delay(2000)
+			.start();
+		})
+		.delay(0)
+		.start();
+	}
+
+	// 更新
+	update(dt) {
+
+	}
+
+	// 描画
+	render(dt) {
+
+	}
+
+}
+
---
+++ docs/js/main.js
@@ -0,0 +1,99 @@
+var camera, renderer;
+var source, context;
+
+var clock = new THREE.Clock();
+var scene = new THREE.Scene();
+var app = new App(scene);
+var loader = new THREE.JSONLoader();
+
+init();
+animate();
+
+function init() {
+	// シーンの作成
+	scene = new THREE.Scene();
+
+	// レンダラの作成
+	renderer = new THREE.WebGLRenderer({
+	  antialias: true,
+	  alpha: true,
+	});
+
+	renderer.setClearColor(new THREE.Color("black"), 0);
+	renderer.setSize(640, 480);
+	renderer.domElement.style.position = "absolute";
+	renderer.domElement.style.top = "0px";
+	renderer.domElement.style.left = "0px";
+	document.body.appendChild(renderer.domElement);
+
+	camera = new THREE.Camera();
+	scene.add(camera);
+
+	// ArToolkitSourceの作成
+	source = new THREEx.ArToolkitSource({
+		sourceType: "webcam",
+	});
+
+	// ArToolkitContextの作成
+	context = new THREEx.ArToolkitContext({
+		debug: false,
+		cameraParametersUrl: "./data/camera_para.dat",
+		detectionMode: "mono",
+		imageSmoothingEnabled: true,
+		maxDetectionRate: 60,
+		canvasWidth: source.parameters.sourceWidth,
+		canvasHeight: source.parameters.sourceHeight,
+	});
+	context.init(function onCompleted(){
+		camera.projectionMatrix.copy(context.getProjectionMatrix());
+	});
+
+	window.addEventListener("resize", function() {
+		resize();
+	});
+
+	window.addEventListener("mousedown", function(ret) {
+		var mouseX = ret.clientX;                           // マウスのx座標
+		var mouseY = ret.clientY;                           // マウスのy座標
+		mouseX =  (mouseX / window.innerWidth)  * 2 - 1;    // -1 ～ +1 に正規化されたx座標
+		mouseY = -(mouseY / window.innerHeight) * 2 + 1;    // -1 ～ +1 に正規化されたy座標
+		var pos = new THREE.Vector3(mouseX, mouseY, 1);     // マウスベクトル
+		pos.unproject(camera);                              // スクリーン座標系をカメラ座標系に変換
+		// レイキャスタを作成（始点, 向きのベクトル）
+		var ray = new THREE.Raycaster(camera.position, pos.sub(camera.position).normalize());
+		var obj = ray.intersectObjects(scene.children, true);   // レイと交差したオブジェクトの取得
+		if(obj.length > 0) {                                // 交差したオブジェクトがあれば
+		app.touch(obj[0].object.name);                       // タッチされた対象に応じた処理を実行
+		}
+	});
+
+	app.init();
+}
+
+function resize() {
+	source.onResizeElement();
+	source.copyElementSizeTo(renderer.domElement);
+	if(context.arController !== null){
+		source.copyElementSizeTo(context.arController.canvas);
+	}
+}
+
+function update(dt) {
+	resize();
+	camera.updateProjectionMatrix();
+	app.update(dt);
+}
+
+function render(dt) {
+	app.render(dt);
+	renderer.render(scene, camera);
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	if(source.ready === false)    { return; }
+	context.update(source.domElement);
+	TWEEN.update();
+	update(clock.getDelta());
+	render(clock.getDelta());
+}
