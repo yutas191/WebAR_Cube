1711416b0c6d1e53bb67b37740fb7a9c6c18be91
yutas191 <yutas191@gmail.com>
2018-09-17 23:19:42 +0900

    Update WebAR_Cube

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,9 +1,13 @@
 var meshCube;
 var meshRocket;
 
+var marker1, controls1;
+var marker2, controls2;
+
 class App {
-	constructor(scene) {
+	constructor(scene,context) {
 		this.scene = scene;
+		this.context = context;
 	}
 
 	init() {
@@ -13,6 +17,35 @@ class App {
 		scene.add( light );
 		var ambientLight = new THREE.AmbientLight(0x888888);
 		scene.add( ambientLight );
+
+		// マーカ1
+		marker1 = new THREE.Group();
+		controls1 = new THREEx.ArMarkerControls(context, marker1, {type: "pattern",patternUrl: "./data/kanji.patt"});
+		scene.add(marker1);
+
+		// マーカ2
+		marker2 = new THREE.Group();
+		controls2 = new THREEx.ArMarkerControls(context, marker2, {type: "pattern",patternUrl: "./data/hiro.patt"});
+		scene.add(marker2);
+
+		// Cube
+		var geometryCube = new THREE.CubeGeometry(1, 1, 1);
+		var materialCube = new THREE.MeshNormalMaterial({transparent: true,opacity: 0.8,side:THREE.DoubleSide});
+		meshCube = new THREE.Mesh(geometryCube, materialCube);
+		meshCube.name = "cube";
+		meshCube.position.set(0, 0.5, 0);
+		marker1.add(meshCube);
+
+		// Rocket
+		loader.load("./model/rocketX.json", function(geometryRocket, materialRocket){
+			materialRocket = new THREE.MeshPhongMaterial({map:THREE.ImageUtils.loadTexture("./model/rocketX.png"),side:THREE.DoubleSide});
+			meshRocket = new THREE.Mesh(geometryRocket, materialRocket);
+			meshRocket.name = "rocket";
+			meshRocket.scale.set(0.3, 0.3, 0.3);
+			meshRocket.position.set(0, 0.5, 0);
+			marker2.add(meshRocket);
+		});
+
 	}
 
 	// タッチ
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,10 @@
 var camera, renderer;
 var source, context;
 
-var marker1, controls1;
-var marker2, controls2;
 
 var clock = new THREE.Clock();
 var scene = new THREE.Scene();
-var app = new App(scene);
+var app = new App(scene,context);
 var loader = new THREE.JSONLoader();
 
 init();
@@ -72,34 +70,6 @@ function init() {
 	// コンテクスト初期化
 	context.init(function onCompleted(){
 		camera.projectionMatrix.copy(context.getProjectionMatrix());
-	});
-
-	// マーカ1
-	marker1 = new THREE.Group();
-	controls1 = new THREEx.ArMarkerControls(context, marker1, {type: "pattern",patternUrl: "./data/kanji.patt"});
-	scene.add(marker1);
-
-	// マーカ2
-	marker2 = new THREE.Group();
-	controls2 = new THREEx.ArMarkerControls(context, marker2, {type: "pattern",patternUrl: "./data/hiro.patt"});
-	scene.add(marker2);
-
-	// Cube
-	var geometryCube = new THREE.CubeGeometry(1, 1, 1);
-	var materialCube = new THREE.MeshNormalMaterial({transparent: true,opacity: 0.8,side:THREE.DoubleSide});
-	meshCube = new THREE.Mesh(geometryCube, materialCube);
-	meshCube.name = "cube";
-	meshCube.position.set(0, 0.5, 0);
-	marker1.add(meshCube);
-
-	// Rocket
-	loader.load("./model/rocketX.json", function(geometryRocket, materialRocket){
-		materialRocket = new THREE.MeshPhongMaterial({map:THREE.ImageUtils.loadTexture("./model/rocketX.png"),side:THREE.DoubleSide});
-		meshRocket = new THREE.Mesh(geometryRocket, materialRocket);
-		meshRocket.name = "rocket";
-		meshRocket.scale.set(0.3, 0.3, 0.3);
-		meshRocket.position.set(0, 0.5, 0);
-		marker2.add(meshRocket);
 	});
 
 	app.init();
